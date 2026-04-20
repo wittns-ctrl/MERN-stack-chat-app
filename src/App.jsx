9628119588717787da8a8982e468d4218d93fc04
wittns <mugishawitness15@gmail.com>
2026-04-20 21:45:49 +0200

    adding bg image on my homepage

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,18 @@
 import React from 'react'
+import { Route, Routes } from 'react-router-dom'
+import HomePage from './pages/HomePage'
+import LoginPage from './pages/LoginPage'
+import ProfilePage from './pages/ProfilePage'
 
-const App = () => {
+const App = () => { 
   return (
-    <div>
-      
+    <div  className="bg-[url('./src/assets/ct.png')]
+     bg-contain">
+      <Routes>
+        <Route path='/' element={<HomePage /> }/>
+        <Route path='/login' element={<LoginPage /> }/>
+        <Route path='/profile' element={<ProfilePage /> }/>
+      </Routes>
     </div>
   )
 }
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -2,8 +2,8 @@ import React from 'react'
 
 const HomePage = () => {
   return (
-    <div>
-      <h1>Home Page</h1>
+    <div className="border w-full h-screen sm:px-[15%] sm:py-[5%]">
+      
     </div>
   )
 }
